883e02bf42da87f85c2cb241b1f492d671de0295
Faris Aziz <farisaziz12@gmail.com>
2020-11-23 23:24:36 +0100

    feat(testimonials): add testimonials enum

---
+++ content/testimonials.js
@@ -0,0 +1,17 @@
+export const testimonials = [
+  {
+    quote:
+      "Faris is an experienced trainer who motivates his clients to push themselves, as well as being able to adjust any exercise in case of injuries, strains etc. He is passionate and dynamic, as well as rigorous in ensuring that his clients are executing their workouts safely.",
+    client: "Laura",
+  },
+  {
+    quote:
+      "Faris might be young, but he’s extremely talented as a coach. He’s friendly, enthusiastic, and has great knowledge - and what he doesn’t know, he’s happy to find out. He is also great at adapting his coaching to the level and skills of his clients. I highly recommend Faris as a coach.",
+    client: "Anna",
+  },
+  {
+    quote:
+      "Faris is a talented trainer. He does an incredible amount of research in order to offer quality bespoke programs to clients. Also, he doesn't just tell you to do things in a certain way - he explains why. Highly recommend.",
+    client: "Jennifer Brant",
+  },
+];
