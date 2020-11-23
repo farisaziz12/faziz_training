c1a7c2aa890e93aa07b89ed5b62650f155af51d9
Faris Aziz <farisaziz12@gmail.com>
2020-11-23 23:25:20 +0100

    refactor(bio): lazy load image and remove experience import

--- components/Bio.js
+++ components/Bio.js
@@ -1,7 +1,8 @@
 import React from "react";
 import FadeIn from "react-fade-in";
+import LazyLoad from "react-lazyload";
+import { bioContent } from "../content/bioContent";
 import styles from "../styles/Home.module.css";
-import { bioContent, experience } from "../content/bioContent";
 
 export default function Bio() {
   return (
@@ -9,11 +10,15 @@ export default function Bio() {
       <h1>More About Me</h1>
       <div className={styles["bio-card"]}>
         <div>
-          <img
-            className={styles["bio-img"]}
-            src="/images/handstand.jpg"
-            alt=""
-          />
+          <LazyLoad>
+            <FadeIn delay={500}>
+              <img
+                className={styles["bio-img"]}
+                src="/images/handstand.jpg"
+                alt=""
+              />
+            </FadeIn>
+          </LazyLoad>
         </div>
         <div>
           <p className={styles["p-text"]}>{bioContent}</p>
