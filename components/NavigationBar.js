34456a68caa58481e0d779960f8ac0a91965e291
Faris Aziz <farisaziz12@gmail.com>
2020-11-24 23:43:28 +0100

    chore(navigation-bar): remove react hooks imports

--- components/NavigationBar.js
+++ components/NavigationBar.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { useRouter } from "next/router";
 import { auth } from "../config/auth-config";
 import Navbar from "react-bootstrap/Navbar";
