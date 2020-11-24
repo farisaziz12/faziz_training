def51f61f56ac85952e12801ec1dc8ce8c45475a
Faris Aziz <farisaziz12@gmail.com>
2020-11-24 23:42:12 +0100

    feat(list-item): create list item component

---
+++ components/ListItem.js
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+import { motion, AnimatePresence } from "framer-motion";
+import Button from "react-bootstrap/Button";
+import ListItemContent from "./ListItemContent";
+import styles from "../styles/Home.module.css";
+
+export default function ListItem({ content }) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleOpen = (state) => setIsOpen(state);
+
+  return (
+    <div>
+      <motion.li layout initial={{ borderRadius: 10 }}>
+        <div style={{ display: "inline-flex" }}>
+          <h5>{content.name}</h5>
+        </div>
+        <AnimatePresence>
+          {isOpen ? (
+            <div onClick={() => toggleOpen(false)}>
+              <ListItemContent key={content.id} content={content} />
+            </div>
+          ) : (
+            <Button
+              style={{ display: "flex" }}
+              onClick={() => toggleOpen(true)}
+            >
+              More Info
+            </Button>
+          )}
+        </AnimatePresence>
+      </motion.li>
+    </div>
+  );
+}
