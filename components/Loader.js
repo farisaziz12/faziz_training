403664ef3285a2ff26c4894572bc591fb24e7747
Faris Aziz <farisaziz12@gmail.com>
2020-11-25 22:07:27 +0100

    feat(loader): create loader component

---
+++ components/Loader.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+import Lottie from "react-lottie";
+import { defaultOption } from "../functions";
+import loadingSpinner from "../lotties/fitness-loading-spinner.json";
+
+export default function Loader() {
+  return (
+    <div>
+      <Lottie
+        options={defaultOption(loadingSpinner)}
+        height={100}
+        width={100}
+      />
+      <p>Loading...</p>
+    </div>
+  );
+}
