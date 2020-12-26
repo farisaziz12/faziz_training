808c16ec42d45fb0e36f11d7ddd26361b62daa04
Faris Aziz <farisaziz12@gmail.com>
2020-12-26 17:23:48 +0100

    refactor(buy): render checkout based on orderId

--- pages/buy.js
+++ pages/buy.js
@@ -1,35 +1,28 @@
 import { useState, useEffect } from "react";
 import { useRouter } from "next/router";
-import FadeIn from "react-fade-in";
-import Button from "react-bootstrap/Button";
 import MetaData from "../components/MetaData";
 import NavigationBar from "../components/NavigationBar";
-import { getServiceDetails } from "../cms";
-import { post } from "../network";
 import styles from "../styles/Home.module.css";
+import Cart from "../components/Cart";
+import Checkout from "../components/Checkout";
 
 export default function buy() {
-  const [cart, setCart] = useState([]);
+  const [orderId, setOrderId] = useState(undefined);
 
   const router = useRouter();
   const { id } = router.query;
-
-  useEffect(async () => {
-    if (id) {
-      try {
-        const service = await getServiceDetails(id);
-        setCart([...cart, service]);
-      } catch (error) {
-        console.error(error);
-      }
-    }
-  }, [id]);
 
   return (
     <div className={styles.container}>
       <MetaData />
       <NavigationBar />
-      <main className={styles.main}></main>
+      <main className={styles.main}>
+        {orderId ? (
+          <Checkout orderId={orderId} setOrderId={setOrderId} />
+        ) : (
+          <Cart serviceId={id} setOrderId={setOrderId} />
+        )}
+      </main>
     </div>
   );
 }
