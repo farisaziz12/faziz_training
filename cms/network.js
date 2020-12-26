18aad38cfff3676d1fc23c9a9d0db8252e3c955b
Faris Aziz <farisaziz12@gmail.com>
2020-12-26 16:56:55 +0100

    feat(network): create network file with cms paths

---
+++ cms/network.js
@@ -0,0 +1,14 @@
+const testURL = "http://localhost:1337";
+const ProdURL = "https://faziz-training-cms.herokuapp.com";
+
+export const url = testURL;
+
+export const paths = {
+  services: "/services",
+  serviceCategories: "/service-categories",
+  athletes: "/athletes",
+  getAthleteWithEmail: "/athletes/email/",
+  carts: "/carts",
+  orders: "/orders",
+  activeServices: "/active-services",
+};
