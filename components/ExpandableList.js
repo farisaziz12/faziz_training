7f391c80aff5ed107cbfebadf1474a9027cf4028
Faris Aziz <farisaziz12@gmail.com>
2020-12-27 21:13:28 +0100

    feat(components): add type to expandable list

--- components/ExpandableList.js
+++ components/ExpandableList.js
@@ -2,14 +2,14 @@ import React from "react";
 import { motion, AnimateSharedLayout } from "framer-motion";
 import ListItem from "./ListItem";
 
-export default function ExpandableList({ list, listTitle }) {
+export default function ExpandableList({ list, listTitle, type }) {
   return (
     <div>
       <h1 style={{ marginTop: "8%" }}>{listTitle}</h1>
       <AnimateSharedLayout>
         <motion.ul layout initial={{ borderRadius: 25 }}>
           {list.map((item) => (
-            <ListItem key={item.id} content={item} />
+            <ListItem key={item.id} content={item} type={type} />
           ))}
         </motion.ul>
       </AnimateSharedLayout>
