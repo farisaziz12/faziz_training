d0388b591c5eb4de9986cb20498b14cf90c879da
Faris Aziz <farisaziz12@gmail.com>
2020-12-27 21:14:35 +0100

    feat(components): render active service and service differently

--- components/ListItemContent.js
+++ components/ListItemContent.js
@@ -3,10 +3,20 @@ import { auth } from "../config/auth-config";
 import { resolveButtons } from "../cms";
 import { generateKey } from "../functions";
 import { motion } from "framer-motion";
+import { dateParse } from "../functions";
 import styles from "../styles/Home.module.css";
 
-export default function ListItemContent({ content }) {
-  const { description, display_price, id } = content;
+export default function ListItemContent({ content, type, toggleOpen }) {
+  const {
+    description,
+    display_price,
+    id,
+    service_id,
+    activated_date,
+    expiration_date,
+    link,
+    amount_left,
+  } = content;
 
   const [buttons, setButtons] = useState([]);
   const [loggedIn, setLoggedIn] = useState(false);
@@ -25,10 +35,36 @@ export default function ListItemContent({ content }) {
   }, []);
 
   useEffect(() => {
-    resolveButtons(id, loggedIn).then((resolvedButtons) =>
+    resolveButtons(id, loggedIn, type, link).then((resolvedButtons) =>
       setButtons(resolvedButtons, ...buttons)
     );
   }, [loggedIn]);
+
+  const renderDetails = () => {
+    if (type === "service") {
+      return (
+        <>
+          <p className={styles["p-text"]}>{description}</p>
+          <p>{display_price}</p>
+        </>
+      );
+    } else if (type === "active-service") {
+      return (
+        <div>
+          <strong>Activated Date: </strong>
+          <p className={styles["p-text"]}>{dateParse(activated_date)}</p>
+          <strong>Expiration Date: </strong>
+          <p className={styles["p-text"]}>{dateParse(expiration_date)}</p>
+          <p className={styles["p-text"]}>
+            <strong>Remaining: </strong>
+            {amount_left}
+          </p>
+        </div>
+      );
+    } else {
+      return <div style={{ display: "none" }}></div>;
+    }
+  };
 
   return (
     <div>
@@ -37,9 +73,9 @@ export default function ListItemContent({ content }) {
         initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
         exit={{ opacity: 0 }}
+        onClick={() => toggleOpen(false)}
       >
-        <p className={styles["p-text"]}>{description}</p>
-        <p>{display_price}</p>
+        {renderDetails()}
       </motion.div>
       <div className={styles["button-group"]}>
         {buttons.map((Button) => (
