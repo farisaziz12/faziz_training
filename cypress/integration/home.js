4db0e0718867e9a7888b0f974793ba4c0755e26d
Faris Aziz <53216647+farisaziz12@users.noreply.github.com>
2021-02-11 22:05:15 +0100

    feat(cypress-home): update tests

--- cypress/integration/home.js
+++ cypress/integration/home.js
@@ -1,6 +1,6 @@
 describe("Homepage", () => {
   it("Main Home Content", () => {
-    cy.visit("http://localhost:3000/");
+    cy.visit("/");
     cy.contains("Train with me");
     cy.contains("Faris Aziz").scrollIntoView({ offset: { top: 100, left: 0 } });
     cy.contains("More About Me");
@@ -8,20 +8,5 @@ describe("Homepage", () => {
     cy.contains("Health & Fitness Certificates");
     cy.contains("Hours of Training Experience");
     cy.contains("Powered by WOD-WITH-FARIS");
-  });
-  it("Navigation Bar", () => {
-    cy.contains("FAZIZ TRAINING");
-    cy.contains("Home").click();
-    cy.contains("Train with me");
-    cy.contains("Services").click();
-    cy.contains("Loading...");
-    cy.contains("Personal Training");
-    cy.contains("Programming");
-    cy.contains("Powered by WOD-WITH-FARIS");
-    cy.contains("Home").click();
-    cy.contains("Login").click();
-    cy.contains("Email address");
-    cy.contains("Password");
-    cy.contains("Don't have an account? Create one here");
   });
 });
